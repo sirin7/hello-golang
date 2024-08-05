f9f9fb1b71cbca9f1ca32f33b65b916d14c391b6
Svetlava Prokopeva <sirin82430@gmail.com>
2024-08-05 19:14:04 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sirin7/hello-golang
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(hello())
+
+}
+
+func hello() string {
+	return "Hello go"
+}
